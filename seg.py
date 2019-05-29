079e83ea39254bb1a71d74e72b714bda4c2de1fa
Erhard Dinhobl <er@master-studios.net>
2019-05-29 15:52:10 +0200

    html page segmentation

--- seg.py
+++ seg.py
@@ -6,39 +6,65 @@ from lxml.html.soupparser import fromstring
 import lxml
 import requests
 
-class Node:
-  siblings = []
-  data = None
-
-  def addSibling(self, node):
-    self.siblings.append(node)
-
 def nodeAnalyze(node):
   childs = node.getchildren()
-  deepText = node.text_content()
+  deepText = node.text_content().encode("utf-8").strip()
   flatTextRaw = node.text
+  tagName = node.tag
   flatText = ""
   if flatTextRaw != None:
     flatText = flatTextRaw.encode("utf-8").strip()
   return {
-    "childs": childs,
+    "childs": "", #childs,
     "noOfChilds": len(childs),
     "deepText": "", #deepText,
-    "flatText": flatText
+    "deepTextLength": len(deepText),
+    "flatText": flatText,
+    "flatTextLength": len(flatText),
+    "tagName": tagName
   }
 
-def buildCleanTree(node, cleanTree):
-  nodeData = nodeAnalyze(node)
-  for child in nodeData["childs"]:
-    buildCleanTree
+def cleanTree(tree):
+  toRemove = []
+  for child in tree:
+    cleanTree(child)
+    if isinstance(child, lxml.html.HtmlComment):
+      toRemove.append(child)
+    else:
+      nodeData = nodeAnalyze(child)
+      if nodeData["tagName"] == "script":
+        toRemove.append(child)
+      elif nodeData["noOfChilds"] == 0 and nodeData["flatText"] == "":
+        toRemove.append(child)
+      elif nodeData["tagName"] == "img":
+        toRemove.append(child)
+      #else:
+      #  print nodeData
+  for element in toRemove:
+    element.getparent().remove(element)
 
+def iterateTree(tree):
+  nodeData = nodeAnalyze(tree)
+  if nodeData["noOfChilds"] == 0:
+    print "--"
+    processNodeUpwards(tree)
+    print "--"
+  else:
+   for child in tree:
+     iterateTree(child)
+
+def processNodeUpwards(leafNode):
+  nodeData = nodeAnalyze(leafNode)
+  print nodeData
+  if leafNode.getparent() != None:
+    processNodeUpwards(leafNode.getparent())
 
 request = requests.get("https://www.oe24.at/leute/oesterreich/Adieu-Niki-Ganze-Welt-ehrt-Legende/382246711")
 htmlContent = request.text
 tree = lxml.html.fromstring(htmlContent)
+cleanTree(tree)
+iterateTree(tree)
 
-cleanTree = Tree()
-buildCleanTree(tree, cleanTree)
-
-#for node in tree.xpath(".//p"):
+#nodes = tree.xpath(".//*")
+#for node in nodes:
 #  print nodeAnalyze(node)
